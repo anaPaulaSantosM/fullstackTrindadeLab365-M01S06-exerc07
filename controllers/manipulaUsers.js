b8c91f36821321088c556099ff7f0d09a7566c76
Ana Paula dos Santos Marianni <ana.marianni@DESKTOP-P6LCRPC.zoomtecnologia.com.br>
2023-03-31 18:36:52 -0300

    first commit

---
+++ controllers/manipulaUsers.js
@@ -0,0 +1,60 @@
+const userById = (req, res) => {
+
+    
+    app.post('/createUser', (req, res) => {
+    
+    
+        if(req.params.idade < 21) {
+            res.send({mensagem:"Usuário não possui idade suficiente"});
+        }
+    
+        let users = [
+
+            {
+                "id":1,
+                "nome":"Fulano",
+                "idade":18,
+                "cargo":"junior",
+                "senha":"12345678"
+              }
+        ];
+
+        if(req.params.body == ""){
+            res.status(406).send({mensagem:"Está faltando dados para concluir a operação"});
+        }else{
+            res.status(200).send({mensagem:"Criado com sucesso"});
+        }
+    });
+    
+    app.delete('/deletedUser/:id', (req, res) => {
+
+    
+        let users = [
+            {
+                "id":1,
+                "nome":"Fulano",
+                "idade":18,
+                "cargo":"junior",
+                "senha":"12345678"
+              }
+        ];
+    
+        let localiza = users.filter((usuario) => {
+            return usuario.id == req.params.id;
+    
+    });
+    
+    if(localiza.length) {
+        res.status(200).send('DEletado com sucesso.');
+    }else{
+        res.status(406).send({mensagem:"Está faltando dados para concluir a operação."});
+    }
+    });
+
+module.exports = 
+    {
+
+        userById
+
+    }
+};
